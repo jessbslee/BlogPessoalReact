be0121ba6c6f1de6b0c5b5459e3943f0a0f80951
jessbslee <jess.b.silva@gmail.com>
2022-05-24 23:05:57 -0300

    Estrutura do React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import'./Home.css';
+
+function Home(){
+    return (
+    <>
+        <h1 className='titulo'>Home</h1>
+        <img src="https://i0.wp.com/revistakoreain.com.br/wp-content/uploads/2021/07/geracoes-kpop.jpg?w=1920&ssl=1" alt="Gerações do K-pop" className='img'/>
+    </>
+    );
+}
+
+export default Home;
