d698cfcb6175353ade492deb864b1d68a0bd424e
jessbslee <jess.b.silva@gmail.com>
2022-06-06 21:49:13 -0300

    Implementou o GetAll, Post, Put e Delete da model de Tema e Postagem

--- src/components/temas/listatema/ListaTema.tsx
+++ src/components/temas/listatema/ListaTema.tsx
@@ -1,45 +1,65 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import {
-  Box,
-  Card,
-  CardActions,
-  CardContent,
-  Button,
-  Typography,
-} from "@material-ui/core";
-import "./ListaTema.css";
+import React, {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import { Box, Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
+import Tema from '../../../models/Tema';
+import './ListaTema.css';
+import useLocalStorage from 'react-use-localstorage';
+import {useNavigate} from 'react-router-dom';
+import { busca } from '../../../services/Service';
 
 function ListaTema() {
+  const [temas, setTemas] = useState<Tema[]>([])
+  const [token, setToken] = useLocalStorage('token');
+  let navigate = useNavigate();
+
+  useEffect(()=>{
+    if(token == ''){
+      alert("Você precisa estar logado")
+      navigate("/login")
+    }
+  }, [token])
+
+
+  async function getTema(){
+    await busca("/tema", setTemas, {
+      headers: {
+        'Authorization': token
+      }
+    })
+  }
+
+
+  useEffect(()=>{
+    getTema()
+  }, [temas.length])
+
   return (
     <>
-      <Box m={2}>
+    {
+      temas.map(tema =>(
+      <Box m={2} >
         <Card variant="outlined">
           <CardContent>
             <Typography color="textSecondary" gutterBottom>
               Tema
             </Typography>
             <Typography variant="h5" component="h2">
-              Minha descrição
+              {tema.descricao}
             </Typography>
           </CardContent>
           <CardActions>
-            <Box display="flex" justifyContent="center" mb={1.5}>
-              <Link to="" className="text-decorator-none">
+            <Box display="flex" justifyContent="center" mb={1.5} >
+
+              <Link to={`/formularioTema/${tema.id}`} className="text-decorator-none">
                 <Box mx={1}>
-                  <Button
-                    variant="contained"
-                    className="marginLeft"
-                    size="small"
-                    color="primary"
-                  >
+                  <Button variant="contained" className="marginLeft" size='small' color="primary" >
                     atualizar
                   </Button>
                 </Box>
               </Link>
-              <Link to="" className="text-decorator-none">
+              <Link to={`/deletarTema/${tema.id}`} className="text-decorator-none">
                 <Box mx={1}>
-                  <Button variant="contained" size="small" color="secondary">
+                  <Button variant="contained" size='small' color="secondary">
                     deletar
                   </Button>
                 </Box>
@@ -48,8 +68,11 @@ function ListaTema() {
           </CardActions>
         </Card>
       </Box>
+      ))
+      }
     </>
   );
 }
+
 
 export default ListaTema;
